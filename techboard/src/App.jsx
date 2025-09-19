957b027abab3116c5e154674170532a8f5e82665
Thiago-Maximo <142333508+Thiago-Maximo@users.noreply.github.com>
2025-09-19 14:56:04 -0300

    Criado o primeiro componente
    Foi criado o primeiro componente, no arquivo app.jsx.
    Criamos um form.

--- techboard/src/App.jsx
+++ techboard/src/App.jsx
@@ -1,6 +1,19 @@
 
 import './App.css'
 
+function FormulariodeEvento(){
+  return(
+    <form className='form-evento'>
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor='nome'>Qual é o nome do Evento ?</label>
+        <input type="text" id='nome'/>
+      </fieldset>
+    </form>
+  )
+}
+
+
 function App() {
 
   return (
@@ -12,6 +25,8 @@ function App() {
         <section>
           <img src='/imagens/banner.png'></img>
         </section>
+
+        <FormulariodeEvento></FormulariodeEvento>
      </main>
   )
 }
