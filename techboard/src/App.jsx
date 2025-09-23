c9f4b4dba2842b42db75167ef83a5a13defbdcda
Thiago-Maximo <142333508+Thiago-Maximo@users.noreply.github.com>
2025-09-23 12:53:14 -0300

    Criado a Pasta Tema
    Criado a pasta de temas que ira compor o combo box

--- techboard/src/App.jsx
+++ techboard/src/App.jsx
@@ -1,9 +1,38 @@
 
 import './App.css'
 import { FormulariodeEvento } from './componentes/FormularioDeEvento'
+import { Tema } from './componentes/Tema'
 
 
 function App() {
+
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligencia artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    }
+  ]
+
 
   return (
      <main>
@@ -16,6 +45,35 @@ function App() {
         </section>
 
         <FormulariodeEvento></FormulariodeEvento>
+
+        <section>
+          <Tema tema={temas[0]}></Tema>
+        </section>
+
+        
+        <section>
+          <Tema tema={temas[1]}></Tema>
+        </section>
+
+        
+        <section>
+          <Tema tema={temas[2]}></Tema>
+        </section>
+
+        
+        <section>
+          <Tema tema={temas[3]}></Tema>
+        </section>
+
+        
+        <section>
+          <Tema tema={temas[4]}></Tema>
+        </section>
+
+        
+        <section>
+          <Tema tema={temas[5]}></Tema>
+        </section>
      </main>
   )
 }
---
+++ techboard/src/componentes/Tema/index.jsx
@@ -0,0 +1,5 @@
+import './tema.css'
+
+export function Tema({tema}){
+    return <h3>{tema.nome}</h3>
+}
